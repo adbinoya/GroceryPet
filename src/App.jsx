4eee77072a5f50f39f48ccb9ee4c7ae1bb34cbfe
Andrea <201910093@fit.edu.ph>
2023-03-22 02:48:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full bg-white'>
+      <p className='font-extrabold'>Grocery Pet</p>
+    </div>
+  )
+}
+
+export default App
